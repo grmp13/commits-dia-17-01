f8f55f5b577538d46b618652251f55d3256b094c
Gabriel Reis <80278946+grmp13@users.noreply.github.com>
2023-01-17 22:03:17 -0300

    site1

---
+++ JAVASCRIPT/aula5/js/index.js
@@ -0,0 +1,14 @@
+var numero = 0;
+var numeroNecessario = 0;
+
+
+numero = prompt()
+
+numeroNecessario = 450 - numero;
+
+if (numeroNecessario > 0){
+    console.log("faltam " + numeroNecessario)
+}
+else{
+    console.log("passou "+ numeroNecessario)    
+}
